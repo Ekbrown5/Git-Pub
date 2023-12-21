35ee47525cfc2f8e0d466ad9514316e95573b837
Eric <ekbrown55@gmail.com>
2023-12-20 20:44:27 -0500

    third commit

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-
 const express = require('express');
-const drinks = require('./db'); 
+const drinks = require('./db');
+const ejs = require('ejs');
 
 const app = express();
 const port = 3000;
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Gitpub App!');
 });
 
-
+// Create a new route for /drinks
 app.get('/drinks', (req, res) => {
-  res.json(drinks); 
+  res.render('index', { drinks }); // Render index.ejs and pass drinks data
 });
 
 app.listen(port, () => {
