7c4531c317e336a1c8583ed14c2cdffa13975cc4
Eric <ekbrown55@gmail.com>
2023-12-20 20:19:08 -0500

    second commit

---
+++ db.js
@@ -0,0 +1,6 @@
+const drinks = [
+    { id: 1, name: 'Coffee', type: 'Hot' },
+    { id: 2, name: 'Iced Tea', type: 'Cold' },
+  ];
+  
+  module.exports = drinks;
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
+
 const express = require('express');
+const drinks = require('./db'); 
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Gitpub App!');
-  });
-  
-  app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-  });
+  res.send('Welcome to the Gitpub App!');
+});
+
+
+app.get('/drinks', (req, res) => {
+  res.json(drinks); 
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
