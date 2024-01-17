1ba5a095ca64d9d5e7872baee26bea855319a813
Eric <ekbrown55@gmail.com>
2024-01-16 23:16:32 -0500

    pubitup

--- db.js
+++
@@ -1,6 +0,0 @@
-const drinks = [
-    { id: 1, name: 'Coffee', type: 'Hot' },
-    { id: 2, name: 'Iced Tea', type: 'Cold' },
-  ];
-  
-  module.exports = drinks;
--- server.js
+++ server.js
@@ -1,21 +1,32 @@
+// imports express
 const express = require('express');
-const drinks = require('./db');
+// imports the drinks db
+const drinks = require('./models/db');
+// imports the EJS
 const ejs = require('ejs');
-
+// Creating the express application
 const app = express();
+// Sets the port
 const port = 3000;
-
+// imports the drinks db
+const {drinks} = require('mdodels/db'); 
+// sets view engine to EJS
 app.set('view engine', 'ejs');
-
+// Sets up a route
 app.get('/', (req, res) => {
-  res.render('index', { drinks });
+res.send('Welcome to the Gitpub App!')
 });
 
-app.get('/drinks/:id', (req, res) => {
-  const id = req.params.id;
-  res.render('show', { drinks, id });
+// renders the index.ejs file with drinks data
+app.get('/drinks', (req, res) => {
+  res.render('index', {drinks});
+});
+
+app.get('drinks/:id', (req, res) => {
+  res.send(req.params.id);
 });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
